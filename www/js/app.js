b281e1e7c51a53bab6b52c8eff68f6fcd02bfe48
Simon Lacoursiere <lacoursieresimon@gmail.com>
2016-01-30 15:10:54 -0500

    Added localStorage object prototype functions.

--- www/js/app.js
+++ www/js/app.js
@@ -126,3 +126,12 @@ angular.module('grocery', ['ionic', 'grocery.controllers', 'grocery.services'])
   $urlRouterProvider.otherwise('/tab/item');
 
 });
+
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
