4a29394ee6582cd104c8e01e1b977a800a263581
Yann Thibodeau <y.thibodeau1@gmail.com>
2016-01-30 12:59:15 -0500

    Completed base template for item details

--- www/js/Controllers/ItemController.js
+++ www/js/Controllers/ItemController.js
@@ -1,4 +1,8 @@
 angular.module('grocery.controllers')
-.controller('ItemController', function($scope, $stateParams, Items) {
+  .controller('ItemController', function ($scope, $stateParams, Items)
+  {
     $scope.product = Items.get($stateParams.itemId);
-});
+    var pattern = new RegExp(/\d+/);
+    $scope.product.EffectiveStartDate  = pattern.exec($scope.product.EffectiveStartDate)[0];
+    $scope.product.EffectiveEndDate  = pattern.exec($scope.product.EffectiveEndDate)[0];
+  });
