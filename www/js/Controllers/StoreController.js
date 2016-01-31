74883b67c00193ff2b0c7a6bf3afe4cdd2be3036
Simon Lacoursiere <lacoursieresimon@gmail.com>
2016-01-31 11:48:01 -0500

    Test log

--- www/js/Controllers/StoreController.js
+++ www/js/Controllers/StoreController.js
@@ -3,10 +3,12 @@ angular.module('grocery.controllers')
     $scope.init = function(){
         Stores.getForCartItem($stateParams.storeId, $stateParams.bannerCode).then(function(data){
           $scope.store = data;
+          console.log(data);
           var position = new google.maps.LatLng($scope.store.latitude, $scope.store.longitude);
+          console.log(position);
           var mapOptions = { center: position, zoom: 16, mapTypeId: google.maps.MapTypeId.ROADMAP };
             $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
+            console.log($scope.map);
             var marker = new google.maps.Marker({
                 position: position,
                 map: $scope.map,
