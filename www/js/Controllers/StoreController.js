66f1195fececf2202c8a87afc295ff612a259450
Simon Lacoursiere <lacoursieresimon@gmail.com>
2016-01-31 11:44:39 -0500

    YO

--- www/js/Controllers/StoreController.js
+++ www/js/Controllers/StoreController.js
@@ -1,17 +1,17 @@
 angular.module('grocery.controllers')
 .controller('StoreController', function($scope, $stateParams, $ionicLoading, $compile, Stores) {
-    ionic.Platform.ready(function(){
-      Stores.getForCartItem($stateParams.storeId, $stateParams.bannerCode).then(function(data){
+    $scope.init = function(){
+        Stores.getForCartItem($stateParams.storeId, $stateParams.bannerCode).then(function(data){
           $scope.store = data;
           var position = new google.maps.LatLng($scope.store.latitude, $scope.store.longitude);
           var mapOptions = { center: position, zoom: 16, mapTypeId: google.maps.MapTypeId.ROADMAP };
-          $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-          var marker = new google.maps.Marker({
-              position: position,
-              map: $scope.map,
-              title: $scope.store.name
-          });
+            var marker = new google.maps.Marker({
+                position: position,
+                map: $scope.map,
+                title: $scope.store.name
+            });
       });
-    });
+    }
 });
